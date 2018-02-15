02c858e6afa2eaa513b34c598b04bff11ffe7295
Melanie27 <melaniemcganney@gmail.com>
2018-02-15 15:03:42 -0800

    add next button

--- src/components/Album.js
+++ src/components/Album.js
@@ -54,6 +54,14 @@ class Album extends Component {
         this.setSong(newSong);
         this.play(newSong);
     }
+
+    handleNextClick() {
+        const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+        const newIndex = Math.min(4, currentIndex + 1);
+        const newSong = this.state.album.songs[newIndex];
+        this.setSong(newSong);
+        this.play(newSong);
+    }
     
      
     render() {
@@ -98,6 +106,7 @@ class Album extends Component {
            currentSong={this.state.currentSong}
            handleSongClick={() => this.handleSongClick(this.state.currentSong)}
            handlePrevClick={() => this.handlePrevClick()}
+           handleNextClick={() => this.handleNextClick()}
          />
         </section>
       );
